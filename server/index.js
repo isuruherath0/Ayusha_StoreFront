4baab634a5a1a575ee31f3bae2e7b15363394976
Isuru Herath <it21041648@my.sliit.lk>
2023-04-21 02:53:59 +0530

    Fifth commit

--- server/index.js
+++ server/index.js
@@ -37,7 +37,23 @@ const storage = multer.diskStorage({
 
   const upload = multer({ storage });
 
-  
 
 
+//mongoose
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 6001;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    /* ADD DATA ONE TIME */
+    // User.insertMany(users);
+    // Post.insertMany(posts);
+  })
+  .catch((error) => console.log(`${error} did not connect`));
 
