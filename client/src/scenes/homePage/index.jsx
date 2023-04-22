1fe1513ead49f9b3837509be32ffdbb2c7fa6548
Isuru Herath <it21041648@my.sliit.lk>
2023-04-22 19:32:25 +0530

    added non working frontends

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,12 +1,28 @@
+import { Box, useMediaQuery } from "@mui/material";
+import { useSelector } from "react-redux";
+import Navbar from "scenes/navbar";
+import PostsWidget from "scenes/widgets/PostsWidget";
 
 
 const HomePage = () => {
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+  const { _id, picturePath } = useSelector((state) => state.user);
 
   return (
-    <div>
-      Home page
-    </div>
+    <Box>
+      <Navbar />
+      <Box
+        width="100%"
+        padding="2rem 6%"
+        display={isNonMobileScreens ? "flex" : "block"}
+        gap="0.5rem"
+        justifyContent="space-between"
+      >
 
+          <PostsWidget userId={_id} />
+
+      </Box>
+    </Box>
   );
 };
 
